0f9d0247a991267909041c613c5b0120ee25fe72
Oleg <oleg17.k@gmail.com>
2023-08-07 21:32:27 +0300

    hiding warnings

--- next.config.js
+++ next.config.js
@@ -8,25 +8,25 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
-  // webpack: (config) => {
-  //   Object.assign(config.resolve.alias, {
-  //     "@mongodb-js/zstd": false,
-  //     "@aws-sdk/credential-providers": false,
-  //     snappy: false,
-  //     aws4: false,
-  //     "mongodb-client-encryption": false,
-  //     kerberos: false,
-  //     "supports-color": false,
-  //   });
-  //   return config;
-  // },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    };
+  webpack: (config) => {
+    Object.assign(config.resolve.alias, {
+      "@mongodb-js/zstd": false,
+      "@aws-sdk/credential-providers": false,
+      snappy: false,
+      aws4: false,
+      "mongodb-client-encryption": false,
+      kerberos: false,
+      "supports-color": false,
+    });
     return config;
   },
+  // webpack(config) {
+  //   config.experiments = {
+  //     ...config.experiments,
+  //     topLevelAwait: true,
+  //   };
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
